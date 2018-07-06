29a1b3e77e79e128f9fb610e7e62b24584775959
Gleb <naumenko.gs@gmail.com>
2018-07-06 15:52:02 -0700

    refactoring

--- main.c
+++ main.c
@@ -2,22 +2,21 @@
 #include <inttypes.h>
 #include <x86intrin.h>
 #include <time.h>
+#include <assert.h>
 
 
 uint64_t mul(uint64_t a, uint64_t b) {
-    const __m128i LOWER_BITS_MASK = _mm_set_epi64x(0, -1);
-    const uint64_t LOW_POLY = ((uint64_t)1 << 4) | (1 << 3) | (1 << 1) | 1;
-    const __m128i LOW_MODULO = _mm_cvtsi64_si128(LOW_POLY);
+    const __m128i LOW_MODULO = _mm_set_epi64x(0, ((uint64_t)1 << 4) | (1 << 3) | (1 << 1) | 1);
 
     __m128i a1 = _mm_cvtsi64_si128(a);
     __m128i b1 = _mm_cvtsi64_si128(b);
     
     __m128i product1 = _mm_clmulepi64_si128(a1, b1, 0x00);
-    __m128i result = _mm_and_si128(product1, LOWER_BITS_MASK);
+    __m128i result = product1;
     
     __m128i product2 = _mm_clmulepi64_si128(LOW_MODULO, product1, 0xF2);
-    result = _mm_xor_si128(result, _mm_and_si128(product2, LOWER_BITS_MASK));
-    __m128i product3 = _mm_clmulepi64_si128(LOWER_BITS_MASK, product2, 0xF2);
+    result = _mm_xor_si128(result, product2);
+    __m128i product3 = _mm_clmulepi64_si128(LOW_MODULO, product2, 0xF2);
     
     result = _mm_xor_si128(result, product3);
     return _mm_cvtsi128_si64(result);
@@ -27,7 +26,7 @@ void measure_mul_time(int tries) {
     uint64_t a = (uint64_t)(1) << 33;
     uint64_t b = (uint64_t)(1) << 33;
     
-    int n = 100000;
+    int n = 1000000;
     double total_times = 0;
     clock_t start, end;
     for (int try = 0; try < tries; try++) {
@@ -43,6 +42,7 @@ void measure_mul_time(int tries) {
 }
 
 int main(int argc, const char * argv[]) {
-    measure_mul_time(1000);
+    measure_mul_time(100);
+    assert(mul((uint64_t)(1) << 33, (uint64_t)(1) << 33) == 108);
     return 0;
 }
